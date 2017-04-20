037d29274b7a197424b04d5abcf1105a760eb04f
vikrammmarkiv <vikrammmarkiv@gmail.com>
2017-04-20 23:09:19 +0530

    OUT FOR REVIEWS

--- app/src/main/java/com/digibuddies/nectus/Adapter.java
+++ app/src/main/java/com/digibuddies/nectus/Adapter.java
@@ -53,6 +53,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.cardadapter> {
     public void onBindViewHolder(final cardadapter holder, final int position) {
         final data temp = kdata.get(position);
         holder.tv1.setText(temp.getUname());
+        holder.tv2.setText("Request Pending...");
         myRef.child(temp.getDevid()).child(connections.kid).child("request").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -60,7 +61,6 @@ public class Adapter extends RecyclerView.Adapter<Adapter.cardadapter> {
                 if (dataSnapshot.getValue().toString().equals("ACCEPTED")){
                     holder.tv2.setText(temp.getEmail());
                 }
-                else holder.tv2.setText("Request Pending...");
             }
 
             @Override
